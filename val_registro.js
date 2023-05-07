83e66551cf9e59279b5da78152de04821f5f05a8
puchojjj <125156309+puchojjj@users.noreply.github.com>
2023-05-06 20:33:04 -0400

    final version

--- val_registro.js
+++ val_registro.js
@@ -1,3 +1,4 @@
+// Obtener el formulario y los campos de entrada
 const form = document.querySelector('form');
 const nombresInput = document.getElementById('nombres');
 const apellidosInput = document.getElementById('apellidos');
@@ -5,27 +6,37 @@ const rutInput = document.getElementById('rut');
 const correoInput = document.getElementById('correo');
 const fotoInput = document.getElementById('exampleFormControlFile1');
 
+// Agregar un controlador de eventos de envío al formulario
 form.addEventListener('submit', (event) => {
+  // Verificar el campo de entrada de nombres
   if (nombresInput.value === '') {
     alert('Por favor, ingrese sus nombres.');
     event.preventDefault();
     return;
   }
+  
+  // Verificar el campo de entrada de apellidos
   if (apellidosInput.value === '') {
     alert('Por favor, ingrese sus apellidos.');
     event.preventDefault();
     return;
   }
+
+  // Verificar el campo de entrada de RUT
   if (!/^[0-9]+-[0-9kK]$/.test(rutInput.value)) {
     alert('Por favor, ingrese un RUT válido.');
     event.preventDefault();
     return;
   }
+
+  // Verificar el campo de entrada de correo electrónico
   if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(correoInput.value)) {
     alert('Por favor, ingrese un correo electrónico válido.');
     event.preventDefault();
     return;
   }
+
+  // Verificar el campo de entrada de foto
   if (fotoInput.value === '') {
     alert('Por favor, seleccione una foto de perfil.');
     event.preventDefault();
