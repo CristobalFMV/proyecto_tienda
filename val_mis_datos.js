ffec0d442ecfde5a2ee6d4030578152159ce8c35
payrus61 <129125587+payrus61@users.noreply.github.com>
2023-05-29 22:22:29 -0400

    Hernán
    js mis datos

--- val_mis_datos.js
+++ val_mis_datos.js
@@ -1,38 +1,90 @@
-const form = document.querySelector('form');
-const validarnombresInput = document.getElementById('nombres');
-const validarapellidosInput = document.getElementById('apellidos');
-const validarRUTInput = document.getElementById('rut');
-const validarcorreoelectronicoInput = document.getElementById('correo electronico');
-const confirmarfotoInput = document.getElementById('exampleFormControlFile1');
+$(document).ready(function () {
+  $('#myForm').submit(function (event) {
+    event.preventDefault();
 
-form.addEventListener('submit', (event) => {
-  if (nombresInput.value === '') {
-    alert('Validar los nombres de su usuario.');
-    event.preventDefault();
-    return;
-  }
-  
-  if (apellidosInput.value === '') {
-    alert('Validar los apellidos de su usuario.');
-    event.preventDefault();
-    return;
-  }
+    if ($('#myForm').valid()) {
+      window.location.href = "usuario.html";
+    } else {
+      $('#myForm').find('.is-invalid').first().focus();
+    }
+  });
 
-  if (!/^[0-9]+-[0-9kK]$/.test(RUTInput.value)) {
-    alert('Validar el RUT .');
-    event.preventDefault();
-    return;
-  }
+  $('#myForm').validate({
+    errorClass: 'is-invalid',
+    errorElement: 'small',
+    errorPlacement: function (error, element) {
+      error.addClass('form-text-error');
+      error.insertAfter(element);
+    },
+    rules: {
+      Nombres:'required',
+      Apellidos:'required',
+      Rut:{
+        required: true,
+        validarRut: true
+      },
+      Correoelectronico:{
+        required: true,
+        Email: true
+      },
+      Password:'required',
+      'confirm-password':{
+        required: true,
+        equalTo:'#password'
+      },
+      Dirección:'required'
+    },
+    messages:{
+      Nombres:'Haga el favor de ingresar sus Nombres.',
+      Apellidos:'Haga el favor, ingrese sus Apellidos.',
+      Rut:{
+        required:'Haga el favor de ingresar un Rut válido.'
+      },
+      Correoelectronico:{
+        required:'Haga el favor de ingresar un correo electrónico válido.',
+        Email:'Haga el favor de ingrese un correo electrónico válido.'
+      },
+      Password:'Haga el favor de ingresar su contraseña.',
+      'confirm-password':{
+        required:'Haga el favor de confirmar su contraseña.',
+        equalTo:'Error, las contraseñas no coinciden.'
+      },
+      Dirección:'Haga el favor de ingresar su dirección.'
+    }
+  });
 
-  if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(correoelectronicoInput.value)) {
-    alert('Valide la direccion de correo electrónico establecida.');
-    event.preventDefault();
-    return;
-  }
+  $.validator.addMethod('validarRut', function (value, element) {
+    value = value.replace(/\./g, '').replace('-', '');
+    if (value.match(/^(\d{2})(\d{3}){2}(\w{1})$/)) {
+      return true;
+    } else if (value.match(/^(\d)(\d{3}){2}(\w{0,1})$/)) {
+      return true;
+    } else if (value.match(/^(\d)(\d{3})(\d{0,2})$/)) {
+      return true;
+    } else if (value.match(/^(\d)(\d{0,2})$/)) {
+      return true;
+    }
+    return false;
+  }, 'Haga el favor de ingresar un Rut válido.');
+});
 
-  if (fotoInput.value === '') {
-    alert('Confirme la foto de entrada para su perfil.');
+$('#myForm input').keydown(function (event) {
+  if (event.keyCode == 13) {
     event.preventDefault();
-    return;
+    return false;
   }
+});
+
+$('#rut').on('input', function () {
+  let value = $(this).val().replace(/\./g, '').replace('-', '');
+  if (value.match(/^(\d{2})(\d{3}){2}(\w{1})$/)) {
+    value = value.replace(/^(\d{2})(\d{3})(\d{3})(\w{1})$/, '$1.$2.$3-$4');
+  } else if (value.match(/^(\d)(\d{3}){2}(\w{0,1})$/)) {
+    value = value.replace(/^(\d)(\d{3})(\d{3})(\w{0,1})$/, '$1.$2.$3-$4');
+  } else if (value.match(/^(\d)(\d{3})(\d{0,2})$/)) {
+    value = value.replace(/^(\d)(\d{3})(\d{0,2})$/, '$1.$2.$3');
+  } else if (value.match(/^(\d)(\d{0,2})$/)) {
+    value = value.replace(/^(\d)(\d{0,2})$/, '$1.$2');
+  }
+  $(this).val(value);
 });
