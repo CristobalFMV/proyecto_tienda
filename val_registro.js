43e6648c666d11b76f78bc6688bc86b3a10d631f
Nick11BZ <129084256+Nick11BZ@users.noreply.github.com>
2023-05-29 21:38:18 -0400

    Nicolas Borquez
    Validaciones de usuario y registro

--- val_registro.js
+++ val_registro.js
@@ -1,45 +1,96 @@
-// Obtener el formulario y los campos de entrada
-const form = document.querySelector('form');
-const nombresInput = document.getElementById('nombres');
-const apellidosInput = document.getElementById('apellidos');
-const rutInput = document.getElementById('rut');
-const correoInput = document.getElementById('correo');
-const fotoInput = document.getElementById('exampleFormControlFile1');
+$(document).ready(function () { // Manejar el evento submit del formulario
+  $('#myForm').submit(function (event) {
+    event.preventDefault();
+    // Evitar el envío convencional del formulario
 
-// Agregar un controlador de eventos de envío al formulario
-form.addEventListener('submit', (event) => {
-  // Verificar el campo de entrada de nombres
-  if (nombresInput.value === '') {
-    alert('Por favor, ingrese sus nombres.');
-    event.preventDefault();
-    return;
-  }
-  
-  // Verificar el campo de entrada de apellidos
-  if (apellidosInput.value === '') {
-    alert('Por favor, ingrese sus apellidos.');
-    event.preventDefault();
-    return;
-  }
+    // Realizar validación manualmente
+    if ($('#myForm').valid()) { // Si la validación es exitosa, redirigir a la otra ruta
+      window.location.href = "usuario.html";
+    } else { // Si hay campos vacíos o no válidos, mostrar mensajes de error
+      $('#myForm').find('.is-invalid').first().focus(); // Enfocar el primer campo con error
+    }
+  });
 
-  // Verificar el campo de entrada de RUT
-  if (!/^[0-9]+-[0-9kK]$/.test(rutInput.value)) {
-    alert('Por favor, ingrese un RUT válido.');
-    event.preventDefault();
-    return;
-  }
+  // Configurar las reglas de validación y los mensajes de error
+  $('#myForm').validate({
+    errorClass: 'is-invalid',
+    errorElement: 'small',
+    errorPlacement: function (error, element) {
+      error.addClass('form-text-error');
+      error.insertAfter(element);
+    },
+    rules: {
+      nombres: 'required',
+      apellidos: 'required',
+      rut: {
+        required: true,
+        validarRut: true
+      },
+      correo: {
+        required: true,
+        email: true
+      },
+      password: 'required',
+      'confirm-password': {
+        required: true,
+        equalTo: '#password'
+      },
+      direccion: 'required'
+    },
+    messages: {
+      nombres: 'Por favor, ingrese sus nombres.',
+      apellidos: 'Por favor, ingrese sus apellidos.',
+      rut: {
+        required: 'Por favor, ingrese un RUT válido.'
+      },
+      correo: {
+        required: 'Por favor, ingrese un correo electrónico válido.',
+        email: 'Por favor, ingrese un correo electrónico válido.'
+      },
+      password: 'Por favor, ingrese su contraseña.',
+      'confirm-password': {
+        required: 'Por favor, confirme su contraseña.',
+        equalTo: 'Las contraseñas no coinciden.'
+      },
+      direccion: 'Por favor, ingrese su dirección.'
+    }
+  });
 
-  // Verificar el campo de entrada de correo electrónico
-  if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(correoInput.value)) {
-    alert('Por favor, ingrese un correo electrónico válido.');
-    event.preventDefault();
-    return;
-  }
+  // Agregar regla de validación personalizada para el RUT
+  $.validator.addMethod('validarRut', function (value, element) {
+    value = value.replace(/\./g, '').replace('-', '');
+    if (value.match(/^(\d{2})(\d{3}){2}(\w{1})$/)) {
+      return true;
+    } else if (value.match(/^(\d)(\d{3}){2}(\w{0,1})$/)) {
+      return true;
+    } else if (value.match(/^(\d)(\d{3})(\d{0,2})$/)) {
+      return true;
+    } else if (value.match(/^(\d)(\d{0,2})$/)) {
+      return true;
+    }
+    return false;
+  }, 'Por favor, ingrese un RUT válido.');
+});
 
-  // Verificar el campo de entrada de foto
-  if (fotoInput.value === '') {
-    alert('Por favor, seleccione una foto de perfil.');
+// Evitar el envío del formulario al presionar Enter en los campos de entrada
+$('#myForm input').keydown(function (event) {
+  if (event.keyCode == 13) {
     event.preventDefault();
-    return;
+    return false;
   }
+});
+
+
+$('#rut').on('input', function () {
+  let value = $(this).val().replace(/\./g, '').replace('-', '');
+  if (value.match(/^(\d{2})(\d{3}){2}(\w{1})$/)) {
+    value = value.replace(/^(\d{2})(\d{3})(\d{3})(\w{1})$/, '$1.$2.$3-$4');
+  } else if (value.match(/^(\d)(\d{3}){2}(\w{0,1})$/)) {
+    value = value.replace(/^(\d)(\d{3})(\d{3})(\w{0,1})$/, '$1.$2.$3-$4');
+  } else if (value.match(/^(\d)(\d{3})(\d{0,2})$/)) {
+    value = value.replace(/^(\d)(\d{3})(\d{0,2})$/, '$1.$2.$3');
+  } else if (value.match(/^(\d)(\d{0,2})$/)) {
+    value = value.replace(/^(\d)(\d{0,2})$/, '$1.$2');
+  }
+  $(this).val(value);
 });
